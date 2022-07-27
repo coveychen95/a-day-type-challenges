ad90e10e148815d59c8ef1a2dd7de37c4be255fe
kvchen95 <kvchen95@outlook.com>
2022-07-27 23:50:30 +0800

    feat: comment

--- 00110-medium-capitalize/template.ts
+++ 00110-medium-capitalize/template.ts
@@ -1,6 +1,9 @@
 // think 
 // 1. split string as array
 // think is wrong! not all word but the first string
+
+// 推断 第一个字符 infer
+// 字母大写 Uppercase
 type MyCapitalize<S extends string> = S extends `${infer FirstStr}${infer Rest}` 
   ? `${Uppercase<FirstStr>}${Rest}`
   : S
