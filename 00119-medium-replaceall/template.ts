93c71b26d1b67b24fb8ccfcd4a648627ed39ff31
kvchen95 <kvchen95@outlook.com>
2022-07-31 23:48:08 +0800

    feat: err

--- 00119-medium-replaceall/template.ts
+++ 00119-medium-replaceall/template.ts
@@ -1,1 +1,5 @@
-type ReplaceAll<S extends string, From extends string, To extends string> = any
+type ReplaceAll<S extends string, From extends string, To extends string> = From extends ''
+  ? S
+  : S extends `${infer L}${From}${infer R}`
+      ? ReplaceAll<`${L}${To}${R}`, From, To>
+      : S
