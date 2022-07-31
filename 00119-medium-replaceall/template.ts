07a0b1263c992137ddf47c5d4241a6d9649c2a2f
kvchen95 <kvchen95@outlook.com>
2022-07-31 23:49:09 +0800

    feat:

--- 00119-medium-replaceall/template.ts
+++ 00119-medium-replaceall/template.ts
@@ -1,5 +1,13 @@
-type ReplaceAll<S extends string, From extends string, To extends string> = From extends ''
-  ? S
-  : S extends `${infer L}${From}${infer R}`
-      ? ReplaceAll<`${L}${To}${R}`, From, To>
-      : S
+// err
+// type ReplaceAll<S extends string, From extends string, To extends string> = From extends ''
+//   ? S
+//   : S extends `${infer L}${From}${infer R}`
+//       ? ReplaceAll<`${L}${To}${R}`, From, To>
+//       : S
+
+type ReplaceAll<S extends string, From extends string, To extends string> = 
+ From extends '' 
+  ? S 
+  : S extends `${infer Left}${From}${infer Right}` 
+    ? `${Left}${To}${ReplaceAll<`${Right}`, From , To>}` 
+    : S
