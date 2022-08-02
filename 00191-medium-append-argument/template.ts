b0c2680203d27c6733437f8fb8e8503fbcb42bc6
gang.chen1 <gang.chen1@yintech.cn>
2022-08-02 09:16:16 +0800

    feat: arguments

--- 00191-medium-append-argument/template.ts
+++ 00191-medium-append-argument/template.ts
@@ -1,4 +1,14 @@
 // 1. 限制函数结构
 // 2. 提取参数
 // 3. 添加参数
-type AppendArgument<Fn, A> = any
+
+// 1. 
+//    fn extends Function
+//    fn extends (...args: any) => any
+// 2. 
+//    fn extends (...args: infer Args) => any
+// 3. 
+//    (...arg: [...Args, A]) => any
+type AppendArgument<fn extends (...args: any) => any, A> = fn extends (...args: infer Args) => infer R
+  ? (...arg: [...Args, A]) => R
+  : never
