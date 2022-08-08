689ad4d0d2ff8e56ee0f1f9e95a1cb832a57694c
kvchen95 <kvchen95@outlook.com>
2022-08-08 22:56:46 +0800

    feat: string to union

--- 00531-medium-string-to-union/template.ts
+++ 00531-medium-string-to-union/template.ts
@@ -1,8 +1,23 @@
-type StringToUnion<T extends string> = any
 
-
-type StringToArray<S extends string> = S extends `${infer F}${infer R}`
+type StringToArray<S extends string, arr extends any[] = []> = S extends ''
+? arr
+: S extends `${infer F}${infer R}`
   ? [F, ...StringToArray<R>]
   : never
 
-type test1 = StringToArray<'1234'>
+// ERR!!!!
+// type StringToUnion<T extends string> = StringToArray<T> extends any[] 
+//   ? (StringToArray<T>)['number']
+//   : never
+
+
+
+
+type StringToUnion<T extends string> = T extends `${infer F}${infer Rest}`
+  ? F | StringToUnion<Rest>
+  : never
+
+
+
+
+type test1 = StringToArray<'1234'> // ['1', '2', '3', '4']
