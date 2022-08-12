dc14fcd7d3fa07605286f0be4403e0a1ee5b8359
kvchen95 <kvchen95@outlook.com>
2022-08-13 01:09:56 +0800

    feat: rm index signature

---
+++ 01367-medium-remove-index-signature/test-cases.ts
@@ -0,0 +1,38 @@
+
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type Foo = {
+  [key: string]: any
+  foo(): void
+}
+
+type Bar = {
+  [key: number]: any
+  bar(): void
+  0: string
+}
+
+const foobar = Symbol('foobar')
+type FooBar = {
+  [key: symbol]: any
+  [foobar](): void
+}
+
+type Baz = {
+  bar(): void
+  baz: string
+}
+
+type Baz2 = {
+  [key: string]: any
+  bar(): void
+}
+type cases = [
+  Expect<Equal<RemoveIndexSignature<Foo>, { foo(): void }>>,
+  Expect<Equal<RemoveIndexSignature<Bar>, { bar(): void; 0: string }>>,
+  Expect<Equal<RemoveIndexSignature<FooBar>, { [foobar](): void }>>,
+  Expect<Equal<RemoveIndexSignature<Baz>, { bar(): void; baz: string }>>,
+]
+type t1 = RemoveIndexSignature<Foo>
+type t3 = RemoveIndexSignature<FooBar>
+type t4 = RemoveIndexSignature<Baz2>
