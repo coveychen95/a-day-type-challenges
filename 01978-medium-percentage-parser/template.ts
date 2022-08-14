a087ed1e50c76ce92df1c074b0e3447bf9698c58
kvchen95 <kvchen95@outlook.com>
2022-08-14 23:24:01 +0800

    feat: ans percent parser

--- 01978-medium-percentage-parser/template.ts
+++ 01978-medium-percentage-parser/template.ts
@@ -1,1 +1,5 @@
-type PercentageParser<A extends string> = any
+type PercentageParser<A extends string> = A extends `${infer L}${infer R}`
+  ? L extends '+' | '-'
+    ? R extends `${infer N}%` ? [L, N, '%'] : [L, R, '']
+    : A extends `${infer N}%` ? ['', N, '%'] : ['', A, '']
+  : ['', '', '']
